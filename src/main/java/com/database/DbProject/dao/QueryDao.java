5619b9103a8769a1954c2dc60fbd2b8c812dd9f2
Karan Bheda <bhedakaran1@gmail.com>
2021-10-09 17:33:01 -0400

    redshift connection

--- src/main/java/com/database/DbProject/dao/QueryDao.java
+++ src/main/java/com/database/DbProject/dao/QueryDao.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,11 +24,12 @@ public class QueryDao {
   @Qualifier("mysql")
   DataSource ds;
 
-  private static final String JDBC_URL =
-      "jdbc:redshift://redshift-cluster-1.cmrkw7vai7ws.us-east-2.redshift.amazonaws.com:5439/dev";
+
   private static final String USERNAME = "awsuser";
   private static final String PASSWORD = "CS527Rutgers";
-  private static final String CLASS_NAME = "com.amazon.redshift.jdbc42.Driver";
+  private static final String JDBC_URL =
+      "jdbc:redshift://redshift-cluster-1.cmrkw7vai7ws.us-east-2.redshift.amazonaws.com:5439/dev";
+  private static final String CLASS_NAME = "com.amazon.redshift.jdbc.Driver";
 
   //cached previous result to support pagination
   public static SqlResponse response = new SqlResponse();
@@ -73,37 +74,65 @@ public class QueryDao {
   }
 
   public boolean test() {
-    Connection connection = null;
+    Connection conn = null;
+    Statement stmt = null;
     try {
-      Class.forName(CLASS_NAME);
-    } catch (ClassNotFoundException e) {
-      System.out.println("JDBC Driver class could not loaded");
-      System.out.println(e.getMessage());
-    }
-    Properties properties = getPropertiesForDriverManager();
-    try {
-      System.out.println("Connecting to the database...");
-      connection = DriverManager.getConnection(JDBC_URL, properties);
-      int count = 0;
-      try (PreparedStatement ps = connection.prepareStatement("select * from aisles");
-          ResultSet rs = ps.executeQuery()) {
-        while (rs.next()) {
-          count++;
-        }
-      }
-      System.out.println(count);
-      return true;
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    }
-    return false;
-  }
+      //Dynamically load driver at runtime.
+      //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
+      //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
+      Class.forName("com.amazon.redshift.jdbc.Driver");
 
-  private Properties getPropertiesForDriverManager() {
-    Properties props = new Properties();
-    props.setProperty("user", USERNAME);
-    props.setProperty("password", PASSWORD);
-    return props;
+      //Open a connection and define properties.
+      System.out.println("Connecting to database...");
+      Properties props = new Properties();
+
+      //Uncomment the following line if using a keystore.
+      //props.setProperty("ssl", "true");
+      props.setProperty("user", USERNAME);
+      props.setProperty("password", PASSWORD);
+      conn = DriverManager.getConnection(JDBC_URL, props);
+
+      //Try a simple query.
+      System.out.println("Listing system tables...");
+      stmt = conn.createStatement();
+      String sql;
+      sql = "select * from information_schema.tables;";
+      ResultSet rs = stmt.executeQuery(sql);
+
+      //Get the data from the result set.
+      while (rs.next()) {
+        //Retrieve two columns.
+        String catalog = rs.getString("table_catalog");
+        String name = rs.getString("table_name");
+
+        //Display values.
+        System.out.print("Catalog: " + catalog);
+        System.out.println(", Name: " + name);
+      }
+      rs.close();
+      stmt.close();
+      conn.close();
+    } catch (Exception ex) {
+      //For convenience, handle all errors here.
+      ex.printStackTrace();
+    } finally {
+      //Finally block to close resources.
+      try {
+        if (stmt != null) {
+          stmt.close();
+        }
+      } catch (Exception ex) {
+      }// nothing we can do
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+    System.out.println("Finished connectivity test.");
+    return true;
   }
 }
 
